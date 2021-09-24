14fd5521debbbbd829f138674e23d952ff34c036
charles-spinosa <charles.spinosa@gmail.com>
2021-09-23 22:44:20 -0400

    5

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port = process.env.port || 8080
 // app.get('/')
 // app.use(express.static('./public/OMAS.html'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/OMAS.html')))
+app.get('/', (req, res) => {
+  console.log('found a req')
+  res.sendFile(path.join(__dirname + '/public/OMAS.html'))
+})
 
 app.listen(port)
